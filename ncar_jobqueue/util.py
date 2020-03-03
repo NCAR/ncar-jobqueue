431d1a77fba36f9b734e0f81fd5754f252eb1600
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-03-02 17:07:27 -0700

    use LocalCluster for non-NCAR machines

--- ncar_jobqueue/util.py
+++ ncar_jobqueue/util.py
@@ -15,22 +15,14 @@ def identify_host():
     is_on_casper = casper.search(hostname)
     is_on_hobart = hobart.search(hostname)
 
-    try:
-        if is_on_cheyenne:
-            return "cheyenne"
+    if is_on_cheyenne:
+        return "cheyenne"
 
-        elif is_on_casper:
-            return "casper"
+    elif is_on_casper:
+        return "casper"
 
-        elif is_on_hobart:
-            return "hobart"
+    elif is_on_hobart:
+        return "hobart"
 
-        else:
-            message = (
-                f"Unable to determine which NCAR cluster you are running on..."
-                f"\n\tSupported machines include: cheyenne, casper/dav, hobart."
-            )
-            raise RuntimeError(message)
-
-    except Exception as exc:
-        raise exc
+    else:
+        return "unknown"
