5ae35cc305c2061920e2aa3ccaef9837e38cb712
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-11-17 14:21:14 -0700

    Update test

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,8 +4,7 @@ from ncar_jobqueue.util import identify_host, in_notebook
 
 
 def test_identify_host():
-    with pytest.warns(UserWarning):
-        identify_host()
+    assert 'unknown' == identify_host()
 
 
 def test_in_notebook():
