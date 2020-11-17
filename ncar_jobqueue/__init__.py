5ef66cd91b709b2b02f2c92f9b7d6e47ea3df078
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-11-16 23:48:03 -0700

    Add `no-pragma-cover` flag

--- ncar_jobqueue/__init__.py
+++ ncar_jobqueue/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# flake8: noqa
 """ Top-level module for ncar-jobqueue. """
 from pkg_resources import DistributionNotFound, get_distribution
 
@@ -7,6 +8,6 @@ from .cluster import NCARCluster
 
 try:
     __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+except DistributionNotFound:  # pragma: no cover
     # package is not installed
-    pass
+    _version__ = '0.0.0'  # pragma: no cover
