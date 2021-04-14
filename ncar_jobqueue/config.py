a8acaf14bec12f5a04db8c65cde055999b7eaae6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2021-04-14 12:25:17 -0600

    🔨 Customize ncar-jobqueue configuration to address Casper switch to PBS (#46)

--- ncar_jobqueue/config.py
+++ ncar_jobqueue/config.py
@@ -1,103 +1,49 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+import datetime
 import os
+import pathlib
 import shutil
-from tempfile import mkdtemp
 
 import dask
 import pkg_resources
 import yaml
-from jinja2 import Environment, FileSystemLoader
 
 from .util import identify_host
 
 
-def ensure_file(source, destination=None, comment=True):
-    """
-    Copy file to default location if it does not already exist
-    This tries to move a default configuration file to a default location if
-    if does not already exist.  It also comments out that file by default.
-    This is to be used by downstream modules (like dask.distributed) that may
-    have default configuration files that they wish to include in the default
-    configuration path.
-    Parameters
-    ----------
-    source : string, filename
-        Source configuration file, typically within a source directory.
-    destination : string, directory
-        Destination directory. Configurable by ``DASK_CONFIG`` environment
-        variable, falling back to ~/.config/dask.
-    comment : bool, True by default
-        Whether or not to comment out the config file when copying.
-    """
-
-    if destination is None:
-        destination = dask.config.PATH
-
-    # destination is a file and already exists, never overwrite
-    if os.path.isfile(destination):
-        return
-
-    # If destination is not an existing file, interpret as a directory,
-    # use the source basename as the filename
-    directory = destination
-    destination = os.path.join(directory, os.path.basename(source))
-
-    try:
-        if not os.path.exists(destination):
-            os.makedirs(directory, exist_ok=True)
-            # Atomically create destination.  Parallel testing discovered
-            # a race condition where a process can be busy creating the
-            # destination while another process reads an empty config file.
-            tmp = '%s.tmp.%d' % (destination, os.getpid())
-            with open(source) as f:
-                lines = list(f)
-            if comment:
-                lines = [
-                    '# ' + line if line.strip() and not line.startswith('#') else line
-                    for line in lines
-                ]
-            lines = [os.path.expandvars(line) for line in lines]
-            with open(tmp, 'w') as f:
-                f.write(''.join(lines))
-            try:
-                os.rename(tmp, destination)
-            except OSError:
-                os.remove(tmp)
-    except OSError:
-        pass
+def ensure_file(source, destination_path):
+    """This tries to move a default configuration file to a default location."""
+    if destination_path.exists():
+        last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(destination_path))
+        new_config_time = datetime.datetime.fromisoformat('2021-04-14')
+        if last_modified < new_config_time:
+            shutil.copyfile(source, destination_path)
+    else:
+        shutil.copyfile(source, destination_path)
 
 
-def _generate_config(config_data_file_path, template_file_path):
-
+def configure(destination_path):
     host = identify_host()
+    if host == 'unknown':
+        return
+    with open(destination_path) as f:
+        original_data = yaml.safe_load(f)[host]
+    _, data = original_data.copy().popitem()
+    for key in {'local-directory', 'log-directory'}:
+        directory = data.get(key, None)
+        if directory and isinstance(directory, str):
+            full_path = dask.config.expand_environment_variables(directory)
+            pathlib.Path(full_path).mkdir(parents=True, exist_ok=True)
+    dask.config.update(
+        dask.config.get('jobqueue'), dask.config.expand_environment_variables(original_data)
+    )
 
-    # Load data from YAML into Python dictionary
-    with open(config_data_file_path) as fpt:
-        config_data = yaml.safe_load(fpt)
-    config_data['host'] = host
 
-    # Load Jinja2 template
-    template_dir = os.path.dirname(os.path.abspath(template_file_path))
-    env = Environment(loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
-    template = env.get_template(os.path.basename(template_file_path))
+config_data_file_path = pathlib.Path(
+    pkg_resources.resource_filename('ncar_jobqueue', 'ncar-jobqueue.yaml')
+)
+destination_dir = pathlib.Path(dask.config.PATH)
+destination_path = destination_dir / config_data_file_path.parts[-1]
 
-    # Render the template with data and return it
-    data = template.render(config_data)
-    return data
-
-
-config_data_file_path = pkg_resources.resource_filename('ncar_jobqueue', 'host_configs.yaml')
-template_file_path = pkg_resources.resource_filename('ncar_jobqueue', 'jobqueue_template.yaml')
-temp_dir = mkdtemp()
-
-config_path = os.path.join(temp_dir, 'ncar-jobqueue.yaml')
-destination = dask.config.PATH
-with open(config_path, 'w') as outfile:
-    data = _generate_config(config_data_file_path, template_file_path)
-    data = yaml.safe_load(data)
-    yaml.dump(data, outfile, default_flow_style=False)
-ensure_file(source=config_path, destination=destination, comment=False)
-
-dask.config.collect(paths=[destination])
-dask.config.refresh()
-shutil.rmtree(temp_dir, ignore_errors=True)
+ensure_file(source=config_data_file_path, destination_path=destination_path)
+configure(destination_path)
--- ncar_jobqueue/util.py
+++ ncar_jobqueue/util.py
@@ -1,26 +1,38 @@
 import re
 import socket
+from collections import namedtuple
 
+import dask_jobqueue
+import distributed
 import psutil
+
+Cluster = namedtuple('Cluster', ['type', 'base_class'])
+CLUSTERS = {
+    'hobart': Cluster('pbs', dask_jobqueue.PBSCluster),
+    'izumi': Cluster('pbs', dask_jobqueue.PBSCluster),
+    'cheyenne': Cluster('pbs', dask_jobqueue.PBSCluster),
+    'casper-dav': Cluster('pbs', dask_jobqueue.PBSCluster),
+    'unknown': Cluster('pbs', distributed.LocalCluster),
+}
+cheyenne_login = re.compile(r'cheyenne([0-9]+).cheyenne.ucar.edu')
+cheyenne_compute = re.compile(r'r([a-zA-Z0-9]+).ib0.cheyenne.ucar.edu')
+dav_login = re.compile(r'casper')
+dav_compute = re.compile(r'crhtc([a-zA-Z0-9]+).hpc.ucar.edu')
+hobart = re.compile(r'h([a-zA-Z0-9]+).cgd.ucar.edu')
+izumi = re.compile(r'i([a-zA-Z0-9]+).unified.ucar.edu')
+
+regexes = [
+    ('cheyenne', cheyenne_login),
+    ('cheyenne', cheyenne_compute),
+    ('casper-dav', dav_login),
+    ('casper-dav', dav_compute),
+    ('izumi', izumi),
+    ('hobart', hobart),
+]
 
 
 def identify_host():
     """Function to determine which host the client is running from."""
-    cheyenne_login = re.compile(r'cheyenne([0-9]+).cheyenne.ucar.edu')
-    cheyenne_compute = re.compile(r'r([a-zA-Z0-9]+).ib0.cheyenne.ucar.edu')
-    dav_login = re.compile(r'casper')
-    dav_compute = re.compile(r'crhtc([a-zA-Z0-9]+).hpc.ucar.edu')
-    hobart = re.compile(r'h([a-zA-Z0-9]+).cgd.ucar.edu')
-    izumi = re.compile(r'i([a-zA-Z0-9]+).unified.ucar.edu')
-
-    regexes = [
-        ('cheyenne-login', cheyenne_login),
-        ('cheyenne-compute', cheyenne_compute),
-        ('dav-login', dav_login),
-        ('dav-compute', dav_compute),
-        ('izumi', izumi),
-        ('hobart', hobart),
-    ]
 
     hostname = socket.getfqdn()
     host = 'unknown'
