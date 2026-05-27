246e3643d6613f6d6a756137ccdfedf56f57223b
Michael Levy <mike.levy.work@gmail.com>
2026-05-27 14:10:15 -0600

    Move from pkg_resources to importlib.metadata
    pkg_resources was moved from setuptools in v82.0.0. The recommendation is to
    use importlib.metadata instead.

--- ncar_jobqueue/config.py
+++ ncar_jobqueue/config.py
@@ -5,7 +5,7 @@ import pathlib
 import shutil
 
 import dask
-import pkg_resources
+from importlib.resources import files
 import yaml
 
 from .util import identify_host
@@ -39,9 +39,7 @@ def configure(destination_path):
     )
 
 
-config_data_file_path = pathlib.Path(
-    pkg_resources.resource_filename('ncar_jobqueue', 'ncar-jobqueue.yaml')
-)
+config_data_file_path = files('ncar_jobqueue') / 'ncar-jobqueue.yaml'
 destination_dir = pathlib.Path(dask.config.PATH)
 destination_path = destination_dir / config_data_file_path.parts[-1]
 
